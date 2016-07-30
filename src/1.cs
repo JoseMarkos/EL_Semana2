f591fbf9dad99ec0d771c61e091672335201468f
jmarkos <jkmarcosb@gmail.com>
2016-07-30 13:00:05 -0600

    adding 1

---
+++ src/1.cs
@@ -0,0 +1,27 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string[] letters = new string[3];
+		
+		Console.WriteLine("Ingrese letra1: ");
+		letters[0] = Console.ReadLine();
+		
+		Console.WriteLine("Ingrese letra2: ");
+		letters[1] = Console.ReadLine();
+		
+		Console.WriteLine("Ingrese letra3: ");
+		letters[2] = Console.ReadLine();
+		
+		Array.Reverse(letters);
+		
+		for (int i=0; i < letters.Length; i++)
+		{
+			Console.WriteLine(letters[i]);
+			
+		}
+		
+		
+	}
