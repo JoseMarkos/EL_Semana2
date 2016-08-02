dd697475a73ba79c0b5c0094b42b3038628657a9
jmarkos <jkmarcos@gmail.com>
2016-08-02 22:26:36 +0200

    adding solution

---
+++ src/EL_Semana2/E1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] letters = new string[3];
+
+            Console.WriteLine("Ingrese letra1: ");
+            letters[0] = Console.ReadLine();
+
+            Console.WriteLine("Ingrese letra2: ");
+            letters[1] = Console.ReadLine();
+
+            Console.WriteLine("Ingrese letra3: ");
+            letters[2] = Console.ReadLine();
+
+            ReversePrintArray(letters);
+            EndProgram();
+        }
+
+        static void EndProgram()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+
+        static void ReversePrintArray(string[] array_name)
+        {
+            Array.Reverse(array_name);
+            Console.WriteLine("");
+
+            for (int i = 0; i < array_name.Length; i++)
+            {
+                Console.WriteLine(array_name[i]);
+            }
+        }
+
+    }
+}
---
+++ src/EL_Semana2/E2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string character;
+            int triangle_width;
+
+            Console.WriteLine("Ingrese un numbero: ");
+            character = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el ancho del triangulo: ");
+            triangle_width = Int32.Parse(Console.ReadLine());
+
+            PrintTriangle(character, triangle_width);
+            EndProgram();
+        }
+
+        static void EndProgram()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+        
+        static void PrintTriangle(string character, int triangle_width)
+        {
+            Console.WriteLine(".......................");
+
+            for (int i = triangle_width; i > 0; i--)
+            {
+                PrintTriangleRow(character, i);
+            }
+        }
+
+        static void PrintTriangleRow(string character, int row_width)
+        {
+            for (int i = 0; i < row_width; i++)
+            {
+                Console.Write(character);
+            }
+
+            Console.WriteLine("");
+        }
+    }
+}
+
