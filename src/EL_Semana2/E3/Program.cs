6950fd31c83496e5824adc03964f2a85db22a093
jmarkos <jkmarcos@gmail.com>
2016-08-03 11:54:12 -0600

    adding Project 3

---
+++ src/EL_Semana2/E3/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3
+{
+    class Program
+    {
+        static string user, pass, right_user, right_pass;
+        static int cont = 1;
+   
+        static void Main(string[] args)
+        {
+            right_user = "marcos";
+            right_pass = "123";
+
+            Login();
+            EndProgram();
+        }
+
+        static void EndProgram()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+
+        static void Login()
+        {
+            LoginNotice();
+            Limited(ValidateValue(user, right_user), ValidateValue(pass, right_pass));
+        }
+
+        static void LoginNotice()
+        {
+            Console.WriteLine("Ingrese usuario: ");
+            user = Console.ReadLine();
+
+            Console.WriteLine("Ingrese contrasenha: ");
+            pass = Console.ReadLine();
+
+            Console.WriteLine("");
+        }
+
+        static bool ValidateValue(string characters, string right_value)
+        {
+            if (characters != right_value)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static void Limited(bool value1, bool value2)
+        {            
+            if (cont == 3)
+            {
+                Console.WriteLine("Has superado los intentos fallidos permitidos.");
+            }
+            else if (value1 & value2)
+            {
+                Console.WriteLine("Genial! Has ingresado los datos correctos!!");
+            }
+            else
+            {
+                cont = cont + 1;
+                Login();
+            }
+        }
+    }
+}
+
